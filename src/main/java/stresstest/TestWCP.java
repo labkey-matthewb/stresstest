03a25e365a512e9a46e7e94559f7fb6f188cafc1
Matthew Bellew <matthewb@Labkey.com>
2020-03-26 12:37:15 -0700

    add remaining GET methods

--- src/main/java/stresstest/TestWCP.java
+++ src/main/java/stresstest/TestWCP.java
@@ -78,10 +78,13 @@ public class TestWCP
         if (isBlank(vars.get("studyId")))
         {
             var studyId = ret.body().jsonPath().getString("studies[0].studyId");
+            var studyVersion = ret.body().jsonPath().getString("studies[0].studyVersion");
             if (isNotBlank(studyId))
             {
                 verbose("studyId=" + studyId);
+                verbose("studyVersion=" + studyVersion);
                 vars.put("studyId",studyId);
+                vars.put("studyVersion", studyVersion);
             }
         }
         return ret;
@@ -146,6 +149,11 @@ public class TestWCP
 
     private ExtractableResponse<Response> getStudyInfo(boolean silent)
     {
+        if (silent && isBlank(vars.get("studyId")))
+        {
+            verbose("skipping getResourcesForStudy");
+            return null;
+        }
         return given()
                 .param("studyId", vars.get("studyId"))
             .when()
@@ -179,6 +187,96 @@ public class TestWCP
     }
 
 
+    private ExtractableResponse<Response> getStudyActivityMetadata(boolean silent)
+    {
+        if (silent && (isBlank(vars.get("studyId")) || isBlank(vars.get("activityId")) || isBlank(vars.get("activityVersion"))))
+        {
+            verbose("skipping getStudyActivityMetadata");
+            return null;
+        }
+        return given()
+                .param("studyId", vars.get("studyId"))
+                .param("activityId", vars.get("activityId"))
+                .param("activityVersion", vars.get("activityVersion"))
+            .when()
+                .get("/activity")
+            .then()
+                .statusCode(200)
+            .extract();
+    }
+
+
+    private ExtractableResponse<Response> getStudyDashboardInfo(boolean silent)
+    {
+        if (silent && isBlank(vars.get("studyId")))
+        {
+            verbose("skipping getStudyDashboardInfo");
+            return null;
+        }
+        return given()
+                .param("studyId", vars.get("studyId"))
+            .when()
+                .get("/studyDashboard")
+            .then()
+                .statusCode(200)
+            .extract();
+    }
+
+
+    private ExtractableResponse<Response> getTermsPolicy(boolean silent)
+    {
+        return when()
+                .get("/termsPolicy")
+            .then()
+                .statusCode(200)
+            .extract();
+    }
+
+    private ExtractableResponse<Response> getNotifications(boolean silent)
+    {
+        return given()
+                .param("skip", "0")
+            .when()
+                .get("/notifications")
+            .then()
+                .statusCode(200)
+            .extract();
+    }
+
+    private ExtractableResponse<Response> getAppUpdates(boolean silent)
+    {
+        if (silent && (isBlank(vars.get("app")) || isBlank(vars.get("appVersion"))))
+        {
+            verbose("skipping getAppUpdates");
+            return null;
+        }
+        return given()
+                .param("app", vars.get("app"))
+                .param("appVersion", vars.get("appVersion"))
+            .when()
+                .get("/appUpdates")
+            .then()
+                .statusCode(200)
+            .extract();
+    }
+
+    private ExtractableResponse<Response> getStudyUpdates(boolean silent)
+    {
+        if (silent && (isBlank(vars.get("studyId")) || isBlank(vars.get("studyVersion"))))
+        {
+            verbose("skipping getStudyUpdates");
+            return null;
+        }
+        return given()
+                .param("studyId", vars.get("studyId"))
+                .param("studyVersion", vars.get("studyVersion"))
+            .when()
+                .get("/studyUpdates")// NOTE documentation typo
+            .then()
+                .statusCode(200)
+            .extract();
+    }
+
     public void apiTest()
     {
         getGatewayAppResourcesInfo(false);
@@ -195,17 +293,13 @@ public class TestWCP
 
         getConsentDocument(false);
 
-        // activity
-        //TODO
+        getStudyActivityMetadata(false);
 
-        // studyDashboard
-        //TODO
+        getStudyDashboardInfo(false);
 
-        // termsPolicy
-        //TODO
+        getTermsPolicy(false);
 
-        // notifications
-        //TODO
+        getNotifications(false);
 
         // feedback
         //TODO
@@ -213,41 +307,49 @@ public class TestWCP
         // contactUs
         //TODO
 
-        // appUpdates
-        //TODO
+        // NOTE: app and appVersion need to be provided in stresstest.properties for this method
+        getAppUpdates(true);
 
-        // studyUpdates
-        //TODO
+        // NOTE: studyVersion need to be provided in stresstest.properties for this method
+        getStudyUpdates(true);
 
         System.out.println("apiTest: passed");
     }
 
-    public void randomApiTest()
+    public void randomApiTest(int count)
     {
         List<Function<Boolean,ExtractableResponse<Response>>> fn = Arrays.asList(
-                this::getResourcesForStudy,
-                this::getConsentDocument,
                 this::getActivityList,
+                this::getAppUpdates,
+                this::getConsentDocument,
+                // this::postContactUs,
                 this::getEligibilityConsentMetadata,
+                //this::postFeedback,
                 this::getGatewayAppResourcesInfo,
+                this::getNotifications,
+                this::getResourcesForStudy,
+                this::getStudyActivityMetadata,
+                this::getStudyDashboardInfo,
                 this::getStudyInfo,
-                this::getStudyList
+                this::getStudyList,
+                this::getStudyUpdates,
+                this::getTermsPolicy
         );
         long start = System.currentTimeMillis();
         var r = new Random();
-        for (int i=0 ; i<100 ; i++)
+        for (int i=0 ; i<count ; i++)
         {
             fn.get(r.nextInt(fn.size())).apply(true);
             try {Thread.sleep(10);} catch (InterruptedException x) { /* pass */ }
         }
-        System.out.println("randomApiTest: 100 api calls");
+        System.out.println("randomApiTest: " + count + " api calls");
         System.out.println("randomApiTest: passed in " + (System.currentTimeMillis()-start) + "ms");
     }
 
     public void run()
     {
         apiTest();
-        randomApiTest();
+        randomApiTest(100);
         System.out.println("TestWCP: success");
     }
 }
